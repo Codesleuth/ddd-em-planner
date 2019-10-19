ad2baf3165a0ebea6f6c91b2249546c4348a8680
David Wood <david.p.wood@gmail.com>
2019-10-19 17:15:44 +0100

    switch to aggressive caching

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,14 +1,28 @@
 /* eslint-env serviceworker */
 /* global workbox:false precacheManifest:false */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 workbox.core.skipWaiting()
 workbox.core.clientsClaim()
 
+// Network First
+const networkCaches = [
+  '/$', // Index
+  '/*', // Anything in the same host
+  '.+/*' // Anything in any host
+]
+
+networkCaches.forEach(mask => {
+  workbox.routing.registerRoute(
+    new RegExp(mask),
+    new workbox.strategies.NetworkFirst({ cacheName: 'dynamic' })
+  )
+})
+
 workbox.routing.registerRoute(
   /\/.*/,
-  new workbox.strategies.StaleWhileRevalidate()
+  new workbox.strategies.NetworkFirst({ cacheName: 'dynamic' })
 )
 
-workbox.precacheAndRoute(precacheManifest)
+workbox.precaching.precacheAndRoute(precacheManifest)
